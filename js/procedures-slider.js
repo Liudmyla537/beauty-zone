a04dbebaf6e02d0cfb29730c618bf10087d1563e
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-12-12 11:03:53 +0200

    add section procedures

---
+++ js/procedures-slider.js
@@ -0,0 +1,35 @@
+// Переконатися, що DOM завантажений перед ініціалізацією Swiper
+document.addEventListener('DOMContentLoaded', function() {
+  const swiperElement = document.querySelector(".procedures__card");
+  
+  if (swiperElement) {
+    const swiper = new Swiper(".procedures__card", {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 16,
+
+      breakpoints: {
+        375: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1280: {
+          slidesPerView: 3,
+        },
+      },
+
+      pagination: {
+        el: ".pagination",
+        clickable: true,
+        bulletClass: "pagination-button",
+        bulletActiveClass: "pagination-button-active",
+      },
+      navigation: {
+        nextEl: ".procedures__btn--next",
+        prevEl: ".procedures__btn--prev",
+      },
+    });
+  }
+});
